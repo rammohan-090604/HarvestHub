e70a6e789b2c787c1d9b8182be05d71e2dfbebe2
rammohan-090604 <nunerammohan@gmail.com>
2024-01-09 21:04:10 +0530

    Developed Responsive navbar for HarvestHub

---
+++ src/components/Home/index.js
@@ -0,0 +1,15 @@
+import {Component} from "react";
+import './index.css'
+
+
+class Home extends Component{
+    render(){
+        return(
+            <div>
+                <h1 className="main-heading">Welcome To <br></br> Harvester Hub</h1>
+            </div>
+        )
+    }
+}
+
+export default Home;
